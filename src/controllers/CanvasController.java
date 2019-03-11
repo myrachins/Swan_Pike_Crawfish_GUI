688513b89ced030b87be0249c2d868e0dcbad4db
Maxim <myurachinskiy@edu.hse.ru>
2019-03-11 20:32:10 +0300

    Lines were added to view

--- src/controllers/CanvasController.java
+++ src/controllers/CanvasController.java
@@ -33,10 +33,10 @@ public class CanvasController implements Initializable {
                     clearCanvas();
                     synchronized (truck) {
                         double xShift = canvas.getWidth() / 2 - truck.getX();
-                        double yShift = canvas.getHeight() / 2 - truck.getY();
+                        double yShift = canvas.getHeight() / 2 + truck.getY();
+                        truckHistory.add(new Pair<>(truck.getX(), truck.getY())); // adding new element to history
                         drawHistory(gc, xShift, yShift);
                         drawTruck(gc, truck, xShift, yShift);
-                        truckHistory.add(new Pair<>(truck.getX(), truck.getY())); // adding new element to history
                         creatures.forEach((creature) -> drawCreature(gc, truck, creature, xShift, yShift));
                         truck.notifyAll();
                     }
@@ -77,7 +77,7 @@ public class CanvasController implements Initializable {
     private void drawTruck(GraphicsContext gc, Truck truck, double xShift, double yShift) {
         synchronized (truck) {
             gc.setFill(Color.BLACK);
-            gc.fillOval(truck.getX() + xShift, truck.getY() + yShift,
+            gc.fillOval(truck.getX() + xShift, -truck.getY() + yShift,
                     AppSettings.TRUCK_WIDTH, AppSettings.TRUCK_HEIGHT);
             truck.notifyAll();
         }
@@ -88,7 +88,7 @@ public class CanvasController implements Initializable {
             synchronized (creature) {
                 gc.setFill(creature.getColor());
                 double x = Math.cos(Math.PI * creature.getAngle() / 180f) * MARGIN_FROM_TRUCK + truck.getX();
-                double y = Math.sin(Math.PI * creature.getAngle() / 180f) * MARGIN_FROM_TRUCK  + truck.getY();
+                double y = -Math.sin(Math.PI * creature.getAngle() / 180f) * MARGIN_FROM_TRUCK  - truck.getY();
                 gc.fillOval(x + xShift, y + yShift,
                         AppSettings.CREATURE_WIDTH, AppSettings.CREATURE_HEIGHT);
                 creature.notifyAll();
@@ -99,9 +99,18 @@ public class CanvasController implements Initializable {
 
     private void drawHistory(GraphicsContext gc, double xShift, double yShift) {
         gc.setFill(Color.BROWN);
+        if (truckHistory.size() == 0) {
+            return;
+        }
+        double xPrev = truckHistory.get(0).getKey() + xShift;
+        double yPrev = -truckHistory.get(0).getValue() + yShift;
         for(Pair<Double, Double> coordinate : truckHistory) {
-            gc.fillOval(coordinate.getKey() + xShift, coordinate.getValue() + yShift,
-                    AppSettings.TRUCK_WIDTH, AppSettings.TRUCK_HEIGHT);
+            double x = coordinate.getKey() + xShift;
+            double y = -coordinate.getValue() + yShift;
+            gc.strokeLine(xPrev, yPrev, x, y);
+            gc.fillOval(x, y, AppSettings.TRUCK_WIDTH, AppSettings.TRUCK_HEIGHT);
+            xPrev = x;
+            yPrev = y;
         }
     }
 }
